923c2efae7695be5ca8dfd9df0ae477ec74b1722
Cristiano Abilio <cristianocafr@gmail.com>
2025-09-16 12:09:29 -0300

    feat: multi auth with breeze and create auth for user/admin

--- saas/routes/web.php
+++ saas/routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsUser;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//user's routes
+Route::middleware(['auth', IsUser::class])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+// Admin routes
+Route::prefix('admin')->middleware(['auth', IsAdmin::class])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
